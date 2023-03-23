b28fa1c41b68a723626351280de3b17e5e355606
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-03-23 15:05:15 -0600

    added a sign up with name & email in html

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ app.use(cors())
 
 app.use(express.static(`${__dirname}/public`))
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '9d200ebe8dfc40bca705783989139d45',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.listen(4000, () => {
     console.log('app is up on 4000')
 })
